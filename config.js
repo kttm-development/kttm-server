74f67647e0f7d93152eb4ca9845f910fb28ba222
Tom <thaborak@gmail.com>
2018-06-11 16:27:59 -0400

    travis

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
+        process.env.DATABASE_URL || 'mongodb://dev1:password1@ds016068.mlab.com:16068/kttm',
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
-        'mongodb://localhost/thinkful-backend-test'
+        'mongodb://dev1:password1@ds016068.mlab.com:16068/kttm'
   // DATABASE_URL:
   //     process.env.DATABASE_URL || 'postgres://localhost/thinkful-backend',
   // TEST_DATABASE_URL:
