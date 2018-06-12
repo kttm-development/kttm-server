471d0e17ce025e1a17a498bef5dfcce72507573c
trentduncan <trent.dunc@gmail.com>
2018-06-12 13:12:47 -0700

    mounted strategies and routers to index.js

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const passport = require('passport');
 
 const { PORT, CLIENT_ORIGIN } = require('./config');
 const { dbConnect } = require('./db-mongoose');
+
+const localStrategy = require('./passport/local');
+const jwtStrategy = require('./passport/jwt');
+
+const usersRouter = require('./routes/users');
+// const authRouter = require('./routes/auth');
 
 const app = express();
 
@@ -25,7 +32,11 @@ app.use(
 app.use(express.json());
 
 
+passport.use(localStrategy);
+passport.use(jwtStrategy);
 
+// app.use('/api', authRouter);
+app.use('/api', usersRouter);
 
 
 function runServer(port = PORT) {
