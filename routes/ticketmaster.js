44d1028df18ccb18122160ba3bfd4f8dbcee6399
trentduncan <trent.dunc@gmail.com>
2018-06-14 12:46:27 -0700

    changed location to venue in response

--- routes/ticketmaster.js
+++ routes/ticketmaster.js
@@ -33,7 +33,7 @@ router.get('/concerts/:location/:genre', (req, res, next) => {
         id: item.id,
         name: item.name,
         img: item.images[0].url,
-        location: `${item._embedded.venues[0].name}` + ` ${item._embedded.venues[0].city.name}` + ` ${item._embedded.venues[0].address.line1}`,
+        venue: `${item._embedded.venues[0].name}` + ` ${item._embedded.venues[0].city.name}` + ` ${item._embedded.venues[0].address.line1}`,
         date: item.dates.start.localDate,
         time: item.dates.start.localTime
       };
