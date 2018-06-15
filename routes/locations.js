eb5c5926c67399bd22da6b5dbe1cf1c8174ec658
trentduncan <trent.dunc@gmail.com>
2018-06-15 13:35:09 -0700

    changed endpoint to grab from db

--- routes/locations.js
+++ routes/locations.js
@@ -2,13 +2,19 @@
 
 const express = require('express');
 const router = express.Router();
-const locations = require('../db/seed/dma');
+// const locations = require('../db/seed/dma');
+const Dma = require('../models/location');
 
 
-router.get('/locations', (req,res) => {
-  const locationNames = locations.map(location => {
-    return {location: location.location};
-  });
+// router.get('/locations', (req,res) => {
+//   const locationNames = locations.map(location => {
+//     return {location: location.location};
+//   });
+//   res.json(locationNames);
+// });
+
+router.get('/locations', async (req,res) => {
+  const locationNames = await Dma.find({}, 'location');
   res.json(locationNames);
 });
 
