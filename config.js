28e9894fec04160cf67daf5d056b86eabdebaff6
Tom <thaborak@gmail.com>
2018-06-15 21:48:30 -0400

    env config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN,
   DATABASE_URL:
         process.env.DATABASE_URL || 'mongodb://dev1:password1@ds016068.mlab.com:16068/kttm',
   TEST_DATABASE_URL:
